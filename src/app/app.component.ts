617d59620c450d2de58fe74542bd1631e8330d6d
ramonbezerra <57471737+ramonbezerra@users.noreply.github.com>
2020-06-29 21:58:36 -0300

    define todo model and form with list of todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'devtodo';
+  public title: String = 'Dev Todo';
+  public todos: Todo[] = [];
+  public todoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      name: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(100),
+        Validators.required
+      ])]
+    });
+  }
+
+  add() {
+    const name = this.todoForm.controls['name'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, name, false));
+    this.todoForm.reset();
+  }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,7 @@
+export class Todo {
+    constructor(
+        public id: Number,
+        public name: String,
+        public done: Boolean
+    ) {}
+}
